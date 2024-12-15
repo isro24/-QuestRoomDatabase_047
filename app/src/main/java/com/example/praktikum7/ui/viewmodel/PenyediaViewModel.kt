ad4edab141b59fa62efcaf34e01afb9e8bcefa63
isro24 <alamat_email_anda@example.com>
2024-12-16 01:10:09 +0700

    Menambahkan initializer untuk HomeMhsViewModel, DetaulMhsViewModel, dan UpdateMhsViewModel di PenyediaViewModel

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum7.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.lifecycle.viewmodel.initializer
@@ -14,6 +15,24 @@ object PenyediaViewModel {
                 krsApp().containerApp.repositoryMhs
             )
         }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
     }
 }
 
