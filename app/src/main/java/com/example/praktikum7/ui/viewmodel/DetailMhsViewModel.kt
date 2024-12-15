c3fc7ab1e8e9bf919e7f000b7d893c96da4283b0
isro24 <alamat_email_anda@example.com>
2024-12-16 01:06:24 +0700

    Menambahkan data class DetailUiState dan fun Mahasiswa.toDetailUiEvent

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -55,4 +55,27 @@ class DetailMhsViewModel(
     }
 }
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+// Memindahkan data dari entity ke UI
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nama = nama,
+        nim = nim,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
 
