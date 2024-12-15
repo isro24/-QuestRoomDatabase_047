f6024fe2e946302dd740c195a573e06d2e428166
isro24 <alamat_email_anda@example.com>
2024-12-16 01:04:13 +0700

    Menambahkan class DetailMhsViewModel dan fun deleteMhs

---
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.praktikum7.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum7.data.entity.Mahasiswa
+import com.example.praktikum7.repository.RepositoryMhs
+import com.example.praktikum7.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMhs() {
+        val currentEvent = detailUiState.value.detailUiEvent
+        if (currentEvent != MahasiswaEvent()) { // Validasi agar tidak kosong
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(currentEvent.toMahasiswaEntity())
+            }
+        }
+    }
+}
+
+
