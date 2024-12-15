5c3680dae4be067b536d6307076a5b061adf34cc
isro24 <alamat_email_anda@example.com>
2024-12-16 00:59:34 +0700

    Menambahkan objeck DestinasiHome, DestinasiDetail, DestinasiUpdate pada AlamatNavigasi

--- app/src/main/java/com/example/praktikum7/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/praktikum7/ui/navigation/AlamatNavigasi.kt
@@ -2,4 +2,21 @@ package com.example.praktikum7.ui.navigation
 
 interface AlamatNavigasi {
     val route: String
+
+    object DestinasiHome : AlamatNavigasi {
+        override val route = "home"
+    }
+
+    object DestinasiDetail : AlamatNavigasi {
+        override val route = "detail"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
+    object DestinasiUpdate : AlamatNavigasi {
+        override val route = "update"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
 }
