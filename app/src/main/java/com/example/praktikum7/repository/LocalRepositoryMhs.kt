69941257a475a0bbfaaf3b444a7b606f6e6d803d
isro24 <alamat_email_anda@example.com>
2024-12-16 00:58:27 +0700

    \Menambahkan fun getAllMhs, getMhs, fun delete dan fun update untuk LocalRepositoryMhs

--- app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.praktikum7.repository
 
 import com.example.praktikum7.data.dao.MahasiswaDao
 import com.example.praktikum7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,21 @@ class LocalRepositoryMhs(
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
 }
